bd3184edeba74dc55fa5c0ef306b5283b5cf17fa
JP <85654561+jp524@users.noreply.github.com>
2022-07-10 18:28:00 -0400

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let array = [];
 
+  for (const char of string) {
+    array.push(char);
+  }
+  array = array.reverse();
+  return array.join("");
 };
 
 // Do not edit below this line
