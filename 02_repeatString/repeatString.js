4a9158b4d7b6f638db53741be78660a63c8bf38b
JP <85654561+jp524@users.noreply.github.com>
2022-07-10 18:16:12 -0400

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, n) {
+  let result = "";
 
+  if (n < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < n; i++) {
+      result = result + string;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
