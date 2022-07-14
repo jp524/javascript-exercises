1addd25440c6d8a28f7304cb0c2f32cac65fb84f
JP <85654561+jp524@users.noreply.github.com>
2022-07-13 21:29:57 -0400

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  function isNumber(input) {
+    if (+Number(input) === input) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
+  if (isNumber(a) && isNumber(b) && a > -1 && b > -1) {
+    let sum = 0;
+    let min = a;
+    let max = b;
+
+    if (a > b) {
+      min = b;
+      max = a;
+    }
+
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
