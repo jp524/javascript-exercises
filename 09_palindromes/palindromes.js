3aedd969127aa9503f61083c1c6f5cec5da4c2f2
JP <85654561+jp524@users.noreply.github.com>
2022-07-21 18:29:21 -0400

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const stringNoPunc = string.replace(/\W|\s/g, "").toLowerCase();
+  const l = stringNoPunc.length;
+  for (let i = 0; i < l / 2; i++) {
+    if (stringNoPunc[i] !== stringNoPunc[l - 1 - i]) {
+      return false;
+    };
+  };
+  return true;
 };
 
 // Do not edit below this line
