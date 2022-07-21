7ff9ba7de668067ada1f599365a14f31d366b1ac
JP <85654561+jp524@users.noreply.github.com>
2022-07-21 18:45:48 -0400

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  } else {
+    let array = [0, 1];
+    for (let i = 1; i < n; i++) {
+      array.push(array[i - 1] + array[i]);
+    }
+    return array.pop();
+  }
 };
 
 // Do not edit below this line
