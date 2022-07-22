39a5d5aea169444dab8e02e18bd2d2f00cac1d87
JP <85654561+jp524@users.noreply.github.com>
2022-07-21 21:45:18 -0400

    Solve exercise 12 with map() and sort()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const peopleWithAge = people.map(person => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    person.age = person.yearOfDeath - person.yearOfBirth;
+    return person;
+  })
+  return peopleWithAge.sort((a, b) => a.age > b.age ? 1 : -1)
+                      .pop();
 };
 
 // Do not edit below this line
