8d3c7aafcfac0f2831b655022d55179d9e00e675
JP <85654561+jp524@users.noreply.github.com>
2022-07-21 22:05:29 -0400

    Solve exercise 12 with reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,29 @@
 const findTheOldest = function (people) {
-  const peopleWithAge = people.map(person => {
+  // const peopleWithAge = people.map(person => {
+  //   if (!person.yearOfDeath) {
+  //     person.yearOfDeath = new Date().getFullYear();
+  //   }
+  //   person.age = person.yearOfDeath - person.yearOfBirth;
+  //   return person;
+  // })
+  // return peopleWithAge.sort((a, b) => a.age > b.age ? 1 : -1)
+  //                     .pop();
+
+  let oldest = {
+    name: "",
+    age: 0,
+  }
+  return people.reduce((oldest, person) => {
     if (!person.yearOfDeath) {
       person.yearOfDeath = new Date().getFullYear();
     }
     person.age = person.yearOfDeath - person.yearOfBirth;
-    return person;
-  })
-  return peopleWithAge.sort((a, b) => a.age > b.age ? 1 : -1)
-                      .pop();
+    if (person.age > oldest.age) {
+      oldest.age = person.age;
+      oldest.name = person.name;
+    };
+    return oldest;
+  }, oldest);
 };
 
 // Do not edit below this line
